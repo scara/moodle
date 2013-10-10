29ead4927eb905f445ee63bad0072448001eeba2
Matteo Scaramuccia <moodle@matteoscaramuccia.com>
2013-10-10 22:53:53 +0200

    MDL-nnnnn Enrollment PayPal IPN discarding HTTP 1.0, requiring HTTP Host
    Backport of MDL-35456.
    Community: https://moodle.org/mod/forum/discuss.php?d=241011.

--- enrol/paypal/ipn.php
+++ enrol/paypal/ipn.php
@@ -63,11 +63,12 @@
 
 /// Open a connection back to PayPal to validate the data
 
-    $header = '';
-    $header .= "POST /cgi-bin/webscr HTTP/1.0\r\n";
-    $header .= "Content-Type: application/x-www-form-urlencoded\r\n";
-    $header .= "Content-Length: " . strlen($req) . "\r\n\r\n";
     $paypaladdr = empty($CFG->usepaypalsandbox) ? 'www.paypal.com' : 'www.sandbox.paypal.com';
+    $header = '';
+    $header .= "POST /cgi-bin/webscr HTTP/1.1\r\n";
+    $header .= "Content-Type: application/x-www-form-urlencoded\r\n";
+    $header .= "Host: $paypaladdr\r\n";
+    $header .= "Content-Length: " . strlen($req) . "\r\n\r\n";
     $fp = fsockopen ($paypaladdr, 80, $errno, $errstr, 30);
 
     if (!$fp) {  /// Could not open a socket to PayPal - FAIL
