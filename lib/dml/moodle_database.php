e0c188022382f8ff910df3cb50932a3da9fde01c
Matteo Scaramuccia <moodle@matteoscaramuccia.com>
2017-06-25 22:00:41 +0200

    WIP dml: Escape the column name when replacing text.

--- lib/dml/moodle_database.php
+++ lib/dml/moodle_database.php
@@ -2392,12 +2392,12 @@ abstract class moodle_database {
             return;
         }
 
-        // NOTE: override this methods if following standard compliant SQL
+        // NOTE: override this method if following standard compliant SQL
         //       does not work for your driver.
 
         $columnname = $column->name;
         $sql = "UPDATE {".$table."}
-                       SET $columnname = REPLACE($columnname, ?, ?)
+                       SET $columnname = REPLACE(".$this->sql_column_name_escape($columnname).", ?, ?)
                      WHERE $columnname IS NOT NULL";
 
         if ($column->meta_type === 'X') {
@@ -2407,7 +2407,7 @@ abstract class moodle_database {
             if (core_text::strlen($search) < core_text::strlen($replace)) {
                 $colsize = $column->max_length;
                 $sql = "UPDATE {".$table."}
-                       SET $columnname = " . $this->sql_substr("REPLACE(" . $columnname . ", ?, ?)", 1, $colsize) . "
+                       SET $columnname = " . $this->sql_substr("REPLACE(" . $this->sql_column_name_escape($columnname) . ", ?, ?)", 1, $colsize) . "
                      WHERE $columnname IS NOT NULL";
             }
             $this->execute($sql, array($search, $replace));
