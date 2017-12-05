173394c0dc749251f122b800135b11e92495fccb
Matteo Scaramuccia <moodle@matteoscaramuccia.com>
2017-12-05 23:45:38 +0100

    MDL-60951 session: Do not emit any header in CLI and PHPUnit scrips

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -1379,7 +1379,9 @@ function disable_output_buffering() {
     error_reporting($olddebug);
 
     // Disable buffering in nginx.
-    header('X-Accel-Buffering: no');
+    if (!CLI_SCRIPT && !PHPUNIT_TEST) {
+        header('X-Accel-Buffering: no');
+    }
 
 }
 
